dade7eb2f750fb55dae4e631bd97de4d3785973f
liu <liuhui2583@qq.com>
2018-06-20 19:10:21 +0800

    feat: 新增了 HTML 静态模板文件

---
+++ server/tpl/normal.js
@@ -0,0 +1,25 @@
+module.exports = 
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.bootcss.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.bootcss.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script> 
+    <title>Koa Server HTML</title>
+</head>
+<body>
+   <div class="container">
+       <div class="row">
+           <div class="col-md-8">
+               <h1>Hi Luke</h1>
+               <p>This is Scott</p>
+           </div>
+           <div class="col-md-4">
+               <p>测试静态 HTML 页面</p>
+           </div>
+       </div>
+   </div> 
+</body>
+</html>`
