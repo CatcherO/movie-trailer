826d8434d68e48b603d16af977eecfc05b8b7798
liu <liuhui2583@qq.com>
2018-06-20 21:57:13 +0800

    feat: 增加模板引擎中间件

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const Koa = require('koa')
-const { normalTpl, pugTpl } = require('./tpl')
+const { resolve } = require('path')
+const views = require('koa-views')
 const app = new Koa()
-const pug = require('pug')
+
+app.use(views(resolve(__dirname, './views'), {
+    extension: 'pug'
+}))
+
 app.use(async (ctx, next) => {
-    ctx.type = 'text/html; charset=utf-8'
-    ctx.body = pug.render(pugTpl, {
-        me: 'Luke',
-        you: 'Scott'
-    })
+    await ctx.render('index', {
+        you: 'Luke',
+        me: 'Scott'
+    }) 
 })
 
 app.listen(2333)
