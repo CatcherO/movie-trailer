a6a60b133a260274917749d7c9b24e59eb9e24bd
liu <liuhui2583@qq.com>
2018-06-21 19:40:20 +0800

    feat: 增加子进程跑爬虫脚本

--- server/crawler/trailer-list.js
+++ server/crawler/trailer-list.js
@@ -51,7 +51,8 @@ const sleep = time => new Promise(resolve => {
     })
     browser.close()
 
-    console.log(result)
+    process.send({result})
+    process.exit(0)
 })()
 
  
--- test.js
+++ test.js
@@ -1,9 +1,34 @@
-const puppeteer = require('puppeteer')
+// test puppeteer
+// const puppeteer = require('puppeteer')
 
-;(async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto('https://movie.douban.com/tag/#/?sort=R&range=8,10&tags=%E7%94%B5%E5%BD%B1,%E7%BE%8E%E5%9B%BD')
-  await page.screenshot({path: 'douban.png'})
-  await browser.close()
-})()
+// ;(async () => {
+//   const browser = await puppeteer.launch()
+//   const page = await browser.newPage()
+//   await page.goto('https://movie.douban.com/tag/#/?sort=R&range=8,10&tags=%E7%94%B5%E5%BD%B1,%E7%BE%8E%E5%9B%BD')
+//   await page.screenshot({path: 'douban.png'})
+//   await browser.close()
+// })()
+
+// test child_process
+
+// var exec = require('child_process').exec;
+
+// var ls = exec('ls -l', function (error, stdout, stderr) {
+//   if (error) {
+//     console.log(error.stack);
+//     console.log('Error code: ' + error.code);
+//   }
+//   console.log('Child Process STDOUT: ' + stdout);
+// });
+var exec = require('child_process').exec;
+var child = exec('ls -l');
+
+child.stdout.on('data', function(data) {
+  console.log('stdout: ' + data);
+});
+child.stderr.on('data', function(data) {
+  console.log('stdout: ' + data);
+});
+child.on('close', function(code) {
+  console.log('closing code: ' + code);
+});
