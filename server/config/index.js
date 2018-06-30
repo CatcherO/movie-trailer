534e2534ead2ffd1ee65745c918706c6e5693502
liu <liuhui2583@qq.com>
2018-07-01 00:11:44 +0800

    rm

--- server/config/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'qiniu': {
-        'bucket': 'movie-chiki',
-        'video': 'http://video.icatchero.com',
-        'AK': '2vKcVRF4XSVB2AUJX-u4PugoD3Whu1o2WViJX1Y2',
-        'SK': '1k5amK7yYgw0ZBcQLZYlI4yC8KuVBWdO9lpnshLB'
-    }
-}
